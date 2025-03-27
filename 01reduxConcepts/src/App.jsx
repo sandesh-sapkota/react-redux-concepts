3adbca9d6067b2fde084703afd7bac246efa2612
Sandesh Sapkota <sapkotasandeshlive12@gmail.com>
2025-03-27 17:34:45 +0545

    first commit

---
+++ 01reduxConcepts/src/App.jsx
@@ -0,0 +1,17 @@
+import Counter from "./components/Count";
+import { useDispatch } from "react-redux";
+
+export default function App() {
+  const dispatch = useDispatch();
+  return (
+    <div className="App">
+      <button onClick={(e) => dispatch({ type: "INCREMENT" })}>
+        Increment
+      </button>
+      <Counter />
+      <button onClick={(e) => dispatch({ type: "DECREMENT" })}>
+        Decrement
+      </button>
+    </div>
+  );
+}
